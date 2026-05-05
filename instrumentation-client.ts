72c9835a318324c6a8312b445d36d31001170d48
Alexander Thorup <alexander@thorup.life>
2026-05-05 11:37:13 +0200

    fix(analytics): disable remote config fetch to stop 403 noise

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -5,6 +5,7 @@ add(sessionReplayPlugin({ sampleRate: 1 }))
 
 init(process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY!, {
   serverUrl: 'https://api.eu.amplitude.com/2/httpapi',
+  fetchRemoteConfig: false,
   autocapture: {
     attribution: true,
     pageViews: true,
